ea2a832c5ab4a15fbbaacc6d7273f1d6d7af7ffe
Nickolas Miranda <nickolasmiranda20@gmail.com>
2025-12-03 14:25:58 -0300

    teste

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from './Hero.module.css'; 
+import opalaImage from '../../assets/opala-hero.png'; 
+import { Link } from 'react-router-dom';
+
+const Hero = () => {
+  const orcamento = () => {
+    window.location.href = '/orcamento';
+  }
+  return (
+    <section className={styles.hero}>
+      <div className={styles.heroContent}>
+        <h1 className={styles.title}>
+          Seja Bem-Vindo
+          <span className={styles.brandName}>ACARB</span>
+        </h1>
+        <p className={styles.description}>
+          Encontre mecânicos de confiança perto de você ou conecte-se a novos
+          clientes de forma rápida e segura.
+        </p>
+          <button className={styles.ctaButton} onClick={orcamento} >
+            Preciso de um Mecânico
+          </button>
+      </div>
+      <div className={styles.heroImageContainer}>
+        <img src={opalaImage} alt="Opala azul com capô aberto" className={styles.heroImage} />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
